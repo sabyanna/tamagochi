cd12b37441c02f4540c9df3057fa0e1ae5420a36
Anna Sabransky <anna.sabransky@gmail.com>
2019-05-29 18:13:45 +0200

    feat(TamagochiApplication):CommandLineRunner implemented, iFoxRepository dependency injected

--- src/main/java/com/greenfox/tamagochi/TamagochiApplication.java
+++ src/main/java/com/greenfox/tamagochi/TamagochiApplication.java
@@ -1,13 +1,25 @@
 package com.greenfox.tamagochi;
 
+import com.greenfox.tamagochi.repository.IFoxRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TamagochiApplication {
+public class TamagochiApplication implements CommandLineRunner {
+
+  private IFoxRepository foxRepository;
+
+  public TamagochiApplication(IFoxRepository foxRepository) {
+    this.foxRepository = foxRepository;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TamagochiApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+
+  }
 }
