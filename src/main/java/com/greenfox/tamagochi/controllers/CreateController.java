e86cd8cf0c6cc38e73d744a8fd0abd88a8fe37db
Anna Sabransky <anna.sabransky@gmail.com>
2019-06-03 17:46:22 +0200

    feat(fox):creating fox works

--- src/main/java/com/greenfox/tamagochi/controllers/CreateController.java
+++ src/main/java/com/greenfox/tamagochi/controllers/CreateController.java
@@ -4,10 +4,7 @@ import com.greenfox.tamagochi.Service.DrinkService;
 import com.greenfox.tamagochi.Service.FoodService;
 import com.greenfox.tamagochi.Service.FoxColorService;
 import com.greenfox.tamagochi.Service.FoxService;
-import com.greenfox.tamagochi.model.Drink;
-import com.greenfox.tamagochi.model.Food;
-import com.greenfox.tamagochi.model.Fox;
-import com.greenfox.tamagochi.model.FoxColor;
+import com.greenfox.tamagochi.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.awt.*;
+import java.util.List;
 
 @Controller
 public class CreateController {
@@ -38,17 +36,21 @@ public class CreateController {
     model.addAttribute("fox", new Fox());
     model.addAttribute("colors", foxColorService.findAll());
     model.addAttribute("foods", foodService.findAll());
+    List<Food> myFoods= foodService.findAll();
     model.addAttribute("drinks", drinkService.findAll());
     model.addAttribute("title", "Create a new fox!");
     return "create";
   }
 
   @PostMapping("/create")
-  public String Create(Model model, @RequestParam(required = true) Long id, Fox fox, Drink drink, Food food, FoxColor foxColor, String gender) {
-    if ((foxService.findById(id)==null)) {
+  public String Create(Fox fox, Model model, String name, String gender, Integer drink, Integer food, Integer foxColor) {
+    foxService.save(fox);
+    //foxService.save(new Fox(name, gender, foodService.findById(food), drinkService.findById(drink), foxColorService.findById(foxColor)));
+    //if ((foxService.findById(id)==null)) {
       //currentFox = name;
       //Fox newFox = new Fox(name, color);
-      foxService.save(fox);
+      //foxService.save(fox);
+      /*
       //model.addAttribute("text", foxService.describe(foxService.getFoxByName(name)));
       //model.addAttribute("text", foxService.getFoxByName(name).describe());
       //model.addAttribute("fulltext", foxService.findById(id).getFullness());
@@ -59,8 +61,10 @@ public class CreateController {
       model.addAttribute("colors", foxColorService.findAll());
       model.addAttribute("title", "There is already a fox with that name, please choose a different one!");
       //model.addAttribute("foxName", name);
-      return "create";
-    }
+
+     */
+      return "redirect:/";
+    //}
   }
 
 }
